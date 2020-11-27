7f3813502ca147c4c35eef50f17c0a63abd4b430
django_mecuser <t244.high.t244@gmail.com>
2020-11-27 16:09:21 +0900

    commit-1

--- new_bonjin_quiz/settings.py
+++ new_bonjin_quiz/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p(32m7r(3*#b2rw_d!8%$tpo*ir#q%$)dqw*j-sqzfbr(aq#-x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
